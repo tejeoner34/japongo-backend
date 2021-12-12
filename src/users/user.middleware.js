2efcc8a9a385f4ec78ac3c7c6b191569b822f861
tejeoner34 <atzarco37@gmail.com>
2021-12-12 13:13:14 +0100

    modify password functionality

--- src/users/user.middleware.js
+++ src/users/user.middleware.js
@@ -4,6 +4,7 @@ import { getElementByIdAndPassword } from "./users-model.js";
 
 export async function checkPasswordMiddleware(req, res, next){
     const {email, password} = req.body;
+    console.log(req.body)
     const passEncoded = encondePassword(password);
     const userinfo = await getElementByIdAndPassword(email, passEncoded);
     if(userinfo === null){
--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteFavController, deleteOneUserController, postFavController, retrieveuserInfo } from './users-controller.js';
+import { deleteFavController, deleteOneUserController, postFavController, retrieveuserInfo, updatePassController } from './users-controller.js';
 import { validateJWTAuth } from '../auth/auth.middleware.js';
 import { checkPasswordMiddleware } from './user.middleware.js';
 
@@ -12,6 +12,7 @@ router.route('/')
     // .all(validateJWTAuth)
     .get(validateJWTAuth, retrieveuserInfo)
     .delete(checkPasswordMiddleware, deleteOneUserController)
+    .patch(checkPasswordMiddleware, updatePassController)
 
 router.route('/fav')
     .post(postFavController)
